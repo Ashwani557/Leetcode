7ca7ea82a36eee0c5e4a4486624fe57db4cc37d0
Ashwani Pandey <ashwani.23mip10097@vitbhopal.ac.in>
2026-01-31 19:01:36 +0530

    Add files via upload

---
+++ MathSQRT.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class MathSQRT {
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+
+        int x = sc.nextInt();
+
+        double z = Math.floor(Math.sqrt(x));
+
+        System.out.println(z);
+
+
+        
+    }
+    
+}
