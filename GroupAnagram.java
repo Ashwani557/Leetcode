028147bed02eecd51cd54ead11bd1f226bee2a80
Ashwani Pandey <ashwani.23mip10097@vitbhopal.ac.in>
2026-02-04 16:53:43 +0530

    Add files via upload

---
+++ GroupAnagram.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+
+    public boolean isAnagram(String s, String t) {
+
+        if (s.length() != t.length()) {
+            return false;
+        }
+
+        char[] a = s.toCharArray();
+        char[] b = t.toCharArray();
+
+        Arrays.sort(a);
+        Arrays.sort(b);
+
+        for (int i = 0; i < a.length; i++) {
+            if (a[i] != b[i]) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    String s = sc.next();
+    String t = sc.next();
+}
