4e25fd24a5a285d2dd59b86210cc747b1ed5f9d8
Ashwani Pandey <ayushkurana2283@gmail.com>
2026-02-15 10:54:29 +0530

    Add files via upload

---
+++ MissingElement.java
@@ -0,0 +1,16 @@
+public class MissingElement {
+   
+    public int missingNumber(int[] nums) {
+       int n = nums.length;
+       int eSum = n*(n+1)/2;
+       int aSum = 0;
+       for(int i = 0; i<n;i++){
+        aSum += nums[i];
+
+       }
+       return eSum - aSum;
+       
+    } 
+}
+    
+
