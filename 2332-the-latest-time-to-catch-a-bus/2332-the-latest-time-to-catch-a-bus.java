bd0c12e4f313b329ebb22c1ff71fbe5848b7784e
Ashwani Pandey <ayushkurana2283@gmail.com>
2026-09-07 23:22:05 +0530

    Time: 28 ms (97.78%), Space: 97.1 MB (58.33%) - LeetHub

--- 2332-the-latest-time-to-catch-a-bus/2332-the-latest-time-to-catch-a-bus.java
+++ 2332-the-latest-time-to-catch-a-bus/2332-the-latest-time-to-catch-a-bus.java
@@ -1,56 +1,35 @@
 class Solution {
     public int latestTimeCatchTheBus(int[] buses, int[] passengers, int capacity) {
-        int m = buses.length;
-        int n = passengers.length;
-
         Arrays.sort(buses);
         Arrays.sort(passengers);
-        Set<Integer> set = new HashSet<>();
-        int c=0,ans=0;
-        for(int i=0;i<m;i++)
-            {
-                int k = capacity;
-                for(int j=c;j<n;j++)
-                    {
-                        set.add(passengers[j]);
-                        if(passengers[j]<=buses[i])
-                        {
-                            if(j==n-1)
-                            {
-                                ans = buses[m-1];
-                                while(set.contains(ans))
-                                    {
-                                        ans--;
-                                    }
-                            }
-                            if(k==1 && i==m-1)
-                            {
-                                
-                                ans=passengers[j]-1;
-                                while(set.contains(ans))
-                                    {
-                                        ans--;
-                                    }
-                            }
-                            c++;
-                            k--;
-                        }
-                        else 
-                        {
-                            if(i==m-1)
-                            {
-                                ans=buses[i];
-                                while(set.contains(ans))
-                                    {
-                                        ans--;
-                                    }
-                            }
-                            break;
-                        }
-                        if(k==0)break;
-                        
-                    }
+        int n = buses.length;
+        int m = passengers.length;
+        int i = 0;
+        int ct=0;
+    
+       for(int bus : buses){
+            ct=0;
+            while(i<m && passengers[i]<=bus && ct<capacity){
+                i++;
+                ct++;
             }
-        return ans;
+       }
+       
+       int ans;
+
+       
+       if(ct<capacity){
+        ans=buses[n-1];
+       }
+       //last bus became full]
+       else{
+        ans=passengers[i-1];
+       }
+       i--;
+       while(i>=0 && passengers[i]==ans){
+        ans--;
+        i--;
+       }
+       return ans;
     }
 }
