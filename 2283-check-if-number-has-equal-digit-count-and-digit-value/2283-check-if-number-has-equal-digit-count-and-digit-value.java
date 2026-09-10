f3cb5ab9db8c2db225d86e35e392d6705a81a852
Ashwani Pandey <ayushkurana2283@gmail.com>
2026-09-11 00:03:19 +0530

    Time: 0 ms (100%), Space: 42.9 MB (82.38%) - LeetHub

--- 2283-check-if-number-has-equal-digit-count-and-digit-value/2283-check-if-number-has-equal-digit-count-and-digit-value.java
+++ 2283-check-if-number-has-equal-digit-count-and-digit-value/2283-check-if-number-has-equal-digit-count-and-digit-value.java
@@ -1,18 +1,32 @@
 class Solution {
     public boolean digitCount(String num) {
-        char[] arr = num.toCharArray();
-        for(int i = 0; i < num.length(); i++) {
-            int count = 0;
-            for(int j = 0; j < num.length(); j++) {
-                if(arr[j] - '0' == i) {
-                    count++;
-                }
-            }
-            if(count != arr[i] - '0') {
+        int count[]=new int[10];
+        for(char c:num.toCharArray()){
+            count[c-'0']++;
+        }
+        for(int i=0;i<num.length();i++){
+            if(count[i]!=num.charAt(i)-'0'){
                 return false;
             }
         }
-
         return true;
     }
 }
+// class Solution {
+//     public boolean digitCount(String num) {
+//         char[] arr = num.toCharArray();
+//         for(int i = 0; i < num.length(); i++) {
+//             int count = 0;
+//             for(int j = 0; j < num.length(); j++) {
+//                 if(arr[j] - '0' == i) {
+//                     count++;
+//                 }
+//             }
+//             if(count != arr[i] - '0') {
+//                 return false;
+//             }
+//         }
+
+//         return true;
+//     }
+// }
